b734f53b92047e9c9ed58bc781aa730676de2de8
burstlam <burstlam@gmail.com>
2013-09-27 21:34:39 +0800

    REMOVE PACKAGE by default

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,26 +21,12 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
 
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
     VisualizationWallpapers \
     PhotoTable
-
-PRODUCT_PACKAGES += \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
