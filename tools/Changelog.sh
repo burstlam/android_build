bdf68f94fd6b17d6d6cfb306ac95b4ab34f375d0
metalspring <cgibson16137@gmail.com>
2014-01-10 07:52:25 -0500

    oops

--- tools/Changelog.sh
+++ tools/Changelog.sh
@@ -4,4 +4,4 @@ _file=~/Downloads/OmniChangelogs/$_now/Omni-Changelog-$_now.log
 
 mkdir -p ~/Downloads/OmniChangelogs/$_now
 
-repo forall -pc git log --reverse --no-merges --since=5.day.ago >  $_file
+repo forall -pc git log --reverse --no-merges --since=1.day.ago >  $_file
