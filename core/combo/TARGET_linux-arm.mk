51bae022650e31a58ce9db230cd96fcba224e60e
metalspring <cgibson16137@gmail.com>
2014-01-10 16:05:17 -0500

    Revert "This is not the flag you are looking for..."
    This reverts commit 2592d7aec59133d725d480a61d00f4e436eb54bd.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -171,8 +171,8 @@ ifneq ($(DEBUG_NO_STDCXX11),yes)
 TARGET_GLOBAL_CPPFLAGS += -std=gnu++11
 endif
 ifneq ($(DEBUG_NO_LTO),yes)
-TARGET_GLOABL_CFLAGS += -flto -fno-toplevel-reorder
-TARGET_GLOBAL_CPPFLAGS += -flto -fno-toplevel-reorder
+TARGET_GLOABL_CFLAGS += -flto -fno-unit-at-a-time
+TARGET_GLOBAL_CPPFLAGS += -flto -fno-unit-at-a-time
 endif
 
 # More flags/options can be added here
